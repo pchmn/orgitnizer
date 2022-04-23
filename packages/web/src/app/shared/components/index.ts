089e8b5f735c5f2d7282993111447ba3eea6cea8
pchmn <pchmn.dev@gmail.com>
2022-04-23 15:00:53 +0200

    feat: create icon components

---
+++ packages/web/src/app/shared/components/index.ts
@@ -0,0 +1,6 @@
+export * from './AppLogo/AppLogo';
+export * from './AppLogo/DynamicLogo';
+export * from './icons/ColorPaletteIcon';
+export * from './icons/GithubIcon';
+export * from './icons/SettingsIcon';
+export * from './icons/UserIcon';
