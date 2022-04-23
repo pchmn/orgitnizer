aef06b33a73320dce6a1a4af97763af38404f528
pchmn <pchmn.dev@gmail.com>
2022-04-23 14:57:59 +0200

    feat: create firebase hooks

---
+++ packages/web/src/lib/core/index.ts
@@ -0,0 +1,5 @@
+export * from './firebase';
+export * from './storage';
+export * from './utils/convertObjectToDotNotation';
+export * from './utils/deepPartial.type';
+export * from './utils/dotNotation.type';
