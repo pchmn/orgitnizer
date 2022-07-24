e4d245f21e6f83201ba0b9228e9c352f0d01e25a
pchmn <pchmn.dev@gmail.com>
2022-07-24 12:46:43 +0200

    test(functions): add test config

---
+++ packages/functions/src/test/setupTests.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from '@core/appwrite';
+
+export const responseMock: Response = {
+  send: vi.fn(),
+  json: vi.fn()
+};
+
+export const requestMock: Request = {
+  env: {},
+  headers: {},
+  payload: {}
+};
+
+export const graphqlMock = vi.fn();
+
+export const updateNameMock = vi.fn();
+
+vi.mock('@core/appwrite/initSdk', () => {
+  return {
+    initSdk: vi.fn().mockReturnValue({
+      account: {
+        updateName: updateNameMock
+      }
+    })
+  };
+});
+
+vi.mock('@core/github', () => {
+  return {
+    initGithubGraphql: () => vi.fn(graphqlMock)
+  };
+});
+
+vi.mock('@core/appwrite/response.model.ts', () => {
+  return {
+    send: vi.fn(),
+    json: vi.fn()
+  };
+});
